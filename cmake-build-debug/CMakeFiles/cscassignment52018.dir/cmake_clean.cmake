6e361e11a72be3269e21222fc4dc981ecd1f82af
Maia Grisch <thisisanimosity@hotmail.com>
2018-04-09 17:49:09 -0700

    this should be new code

---
+++ cmake-build-debug/CMakeFiles/cscassignment52018.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cscassignment52018.dir/unique.c.o"
+  "cscassignment52018.pdb"
+  "cscassignment52018"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cscassignment52018.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- unique.c
+++ unique.c
@@ -25,7 +25,8 @@ int main() {
     int count = 0;
     int num_words = 0;
     int i = 0;
-    char tempstring;
+    int n = 0;
+    char tempstring[99];
     while (ch != EOF) {
         while (is_word_character(ch) != 0) {
             words[i] = ch;
@@ -33,13 +34,18 @@ int main() {
             i++;
             count++;
         }
-        words[i+1] = '/0';
-        for( i = 0; i < count ; i++ ) {
-            if (strcmp(array->word[i], words[i]) == 0) {
-                is_unique = 0;
+        words[i+1] = '\0';
+        for( i = 0; words[i] != 0 ; i++ ) {
+            for (n = 0; n < count; n++) {
+                array[n];
+                tempstring[i] = array->word[i];
+                if (strcmp(tempstring, words) == 0) {
+                    is_unique = 0;
+                }
             }
         }
         if(is_unique == 1) {
+            array[num_words];
             array->word[i] = (char) words[i];
             num_words++;
             ch = fgetc(input_text);
@@ -51,7 +57,7 @@ int main() {
         ch = fgetc(input_text);
     }
     for (i = 0; i < num_words; i++){
-        printf("%s", array->word[i]);
+        printf("%s", (array[i].word));
     }
     fclose(input_text);
 
